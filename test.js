0d4c17443c217383bfa37f49051d5e1f076c2acd
Ahmed Akmal <akmal.ahmed525@gmail.com>
2018-05-25 15:58:12 +0530

    Deleted the test.js file

--- test.js
+++
@@ -1,7 +0,0 @@
-// dbconn.bucket.upsert(id, {name:username, title:title, date:date, note:note, type:'todo'}, (err2, res2)=>{
-            //     console.log(res2)
-            // })
-            // dbconn.bucket.query(dbconn.N1qlQuery.fromString('SELECT * FROM `TODO` WHERE `TODO`.`name`="'+username+'" AND `TODO`.`type`="todo"'),
-            // (err, rows)=>{
-            //     res.json({rows})
-            // })
